01f94901a1d53a2ef8c9219545e89db7685f091a
Arie <arie.cbpro@gmail.com>
2016-12-15 21:54:44 -0300

    Inicializa el repositorio

---
+++ Assets/Script/GyroBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+		Input.gyro.updateInterval = 0.01F;
+
+		Debug.Log(Input.gyro.enabled);
+	}
+	
+	void Update () {
+		Vector3 v = Input.gyro.rotationRateUnbiased;
+		transform.Rotate(-v.x, -v.y, v.z);
+	}
+
+}
